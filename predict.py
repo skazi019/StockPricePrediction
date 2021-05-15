83ba860d0836a94f89f848e44dd6753d18718174
skazi019 <skazi019@gmail.com>
2021-05-15 14:28:23 +0530

    Working in the main function

--- predict.py
+++ predict.py
@@ -1,23 +1,42 @@
-import os
+import os, sys, datetime, json, requests
+import pandas_datareader as pdr
 
-print("Hello!\n We are going to predict Stocks!")
+print("\n")
 
 class scrip_predict:
     def __init__(self, scrip):
         self.scrip = scrip
 
-    def get_data(self):
+    #def get_data(self):
         # Gets the data for the scrip given by user
     
-    def preprocess_data(self):
+    #def preprocess_data(self):
         # process the data for the lstm model
     
-    def predict_scrip(self):
+    #def predict_scrip(self):
         # run the model and precit on the data
     
-    def results(self):
+    #def results(self):
         # Return results to the user in the form of a chart opening
         # in a window
     
-class LSTM:
-    # Build the LSTM model using tensorflow
+#class LSTM:
+   # Build the LSTM model using tensorflow
+
+
+if __name__ == "__main__":
+    
+    print("Input the Stock Code you want to checkout!")
+    scrip = input()
+    #print(f"Scrip selected: {scrip}")
+
+    with open('./api_credential.json', 'r') as creds:
+        api_key = creds["API-KEY"]
+
+    API = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NSE:{scrip}&apikey={api_key}&outputsize=full"
+
+    data = requests.get(API)
+    print(data.status_code)
+
+    print(f"Below is the data fetched for {scrip}\n")
+    print(data.text)
