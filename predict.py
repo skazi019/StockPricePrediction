4b1cc1d907dcfd2e5c48f112de800c3f89e95524
skazi019 <skazi019@gmail.com>
2021-05-15 13:55:53 +0530

    Defining the structure of the file and the plan

---
+++ predict.py
@@ -0,0 +1,23 @@
+import os
+
+print("Hello!\n We are going to predict Stocks!")
+
+class scrip_predict:
+    def __init__(self, scrip):
+        self.scrip = scrip
+
+    def get_data(self):
+        # Gets the data for the scrip given by user
+    
+    def preprocess_data(self):
+        # process the data for the lstm model
+    
+    def predict_scrip(self):
+        # run the model and precit on the data
+    
+    def results(self):
+        # Return results to the user in the form of a chart opening
+        # in a window
+    
+class LSTM:
+    # Build the LSTM model using tensorflow
